0653d3902b399901e973023238149be223ac5d41
Yatharth08 <33821668+Yatharth08@users.noreply.github.com>
2024-04-18 18:32:36 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import openai
 import json
@@ -7,10 +8,9 @@ import re
 from sentence_transformers import SentenceTransformer
 import pinecone
 from pinecone import Pinecone, ServerlessSpec, PodSpec
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
 
-openai.api_key  = os.environ['OPENAI_API_KEY']
+
+openai.api_key = <open_ai_key>
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -26,7 +26,7 @@ def get_entity(text):
 
     description_of_parameters = {
         'province': "This will give the province name mentioned in the query",
-        'city': "This will give the city name mentioned in the query",
+        'city': "This will give the city name mentioned in the query. Correct the city spelling if not already correct",
         'location': "Complete address of the city",
         'bedroom': "No of bedrooms",
         'washroom': "No of washrooms",
@@ -39,7 +39,7 @@ def get_entity(text):
     }
 
     prompt = f"""
-    Extract the entity from the given text delimited by triple backticks ```{text}``` defined as list of parameters defined in triple backticks ```{listofparameters}```  \
+    Extract the entity with correct spelling from the given text delimited by triple backticks ```{text}``` defined as list of parameters defined in triple backticks ```{listofparameters}```  \
     and for their context can refer to the  description of parameter dictionary again defined in triple backticks ```{description_of_parameters}. Make sure \
     Provide them in JSON format with the entities as keys and the extracted entity as values, if there is no values extracted for any key assign empty string to it.
     """
@@ -83,7 +83,7 @@ def main():
         st.session_state.model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
     if 'pc' not in st.session_state:
-        st.session_state.pc = Pinecone(api_key='<pinecone key>')
+        st.session_state.pc = Pinecone(api_key=<pinecone_api_key>)
 
     search_query = st.text_input("Search your property")
 
@@ -96,28 +96,57 @@ def main():
             if key in st.session_state.data:
                 st.session_state.data[key] = value
 
+
     if st.button("Find Property"):
         missing_fields = [key for key, value in st.session_state.data.items() if value in [None, ""] and key in ["price", "city", "house_type"]]
         if missing_fields and st.session_state.count:
             st.write(f"Please enter {', '.join(missing_fields)}")
             st.session_state.count = False
         else:
+            st.session_state.count = True
             descriptions = [search_query]
             embeddings = st.session_state.model.encode(descriptions).tolist()
 
-            index = st.session_state.pc.Index("iloreai")
+            index = st.session_state.pc.Index("hs")
             result = []
             temp = []
-            results = index.query(vector=embeddings,top_k=3,include_metadata=True)
 
-            for itr2 in results['matches']:
-              if itr2['score'] > 0.85:
-                description = next(iter(itr2['metadata'].values())).replace("dict_values(['", "").replace("'])", "").strip('"')
-                st.markdown(description)
+            results = index.query(vector=embeddings,top_k=5,include_metadata=True)
+            #st.write(results)
 
+            property_list = []
+            for i, match in enumerate(results['matches'], start=1):
+                metadata = match['metadata']
+                address = metadata['address']
+                description = metadata['description']
+                photo_url = metadata['photo']
 
+                if st.session_state.data["city"].lower() == metadata["city"].lower():
+                    property_list.append({
+                        "address": address,
+                        "description": description,
+                        "photo_url": photo_url
+                    })
+            if property_list:
+                for i, property_data in enumerate(property_list, start=1):
+                    st.write(f"{i}. Address: {property_data['address']}")
+                    st.write(f"Description: {property_data['description']}")
+                    st.image(property_data['photo_url'], caption='Property Photo', use_column_width=True)
+            else:
+                st.write("No property found in this city")
 
+            st.session_state.data = {
+              "province": "",
+              "city": "",
+              "location": "",
+              "bedroom": "",
+              "washroom": "",
+              "garage": "",
+              "price": "",
+              "area": "",
+              "basement": "",
+              "house_type": "",
+              "status": ""
+            }
 if __name__ == "__main__":
     main()
-
-
